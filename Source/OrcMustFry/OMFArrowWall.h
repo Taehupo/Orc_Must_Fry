a8e1f48ae05043229b0dfa388bf56222b008a071
Aubert gabriel <uzumakigip@gmail.com>
2018-11-23 12:51:10 +0100

    Added ArrowTrap
    Arrow trap is functional. Designers variables to adjust the inner workings of the trap.

--- Source/OrcMustFry/OMFArrowWall.h
+++ Source/OrcMustFry/OMFArrowWall.h
@@ -13,8 +13,30 @@ UCLASS()
 class ORCMUSTFRY_API AOMFArrowWall : public AOMFTrap
 {
 	GENERATED_BODY()
+
+	bool isShooting;
+
+	UPROPERTY(VisibleAnywhere, Category = OMFTrap)
+	int32 nbProjectileShot;
+
+	UPROPERTY(EditAnywhere, Category = OMFTrap)
+	float cooldown = 1.5f;
+
+	float timer = 1.5f;
+
+public:
+	UPROPERTY(EditAnywhere, Category = OMFTrap)
+	int32 nbProjectileToShoot;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = OMFTrap)
+	TSubclassOf<class AOMFProjectile> ProjectileClass;
 	
-	
-	
+public:
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void OnValidBeginOverlap(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	virtual void OnValidEndOverlap(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 	
 };
