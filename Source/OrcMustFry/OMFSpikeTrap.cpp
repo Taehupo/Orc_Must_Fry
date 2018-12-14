9c8ec4c4b80c13ee5af87bb02368617d0601eb6b
Aubert gabriel <uzumakigip@gmail.com>
2018-12-14 14:10:35 +0100

    Del to Update to correction
    Del

--- Source/OrcMustFry/OMFSpikeTrap.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "OMFSpikeTrap.h"
-
-void AOMFSpikeTrap::OnValidBeginOverlap(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	UE_LOG(LogTemp, Log, TEXT("I HAZ OVERLAPPED ! I IZ SPIKY !"));
-}
